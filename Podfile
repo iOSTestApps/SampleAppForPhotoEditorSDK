0d7b9c8e8ffae4bf82a122f6b7dd439347ed9313
Romain Pouclet <romain.pouclet@gmail.com>
2018-04-27 15:11:12 -0400

    Create a sample app with 'PhotoEditorSDK'

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SampleAppForPhotoEditorSDK' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'PhotoEditorSDK', '~> 8.3'
+
+end
--- SampleAppForPhotoEditorSDK/AppDelegate.swift
+++ SampleAppForPhotoEditorSDK/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PhotoEditorSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if let licenseURL = Bundle.main.url(forResource: "license", withExtension: "") {
+            PESDK.unlockWithLicense(at: licenseURL)
+        }
+
         return true
     }
 
